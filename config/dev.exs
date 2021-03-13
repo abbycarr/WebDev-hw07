30e47f54e6c614b6715d1185abb6ff1d69f16d6a
abbycarr <carr.ab@husky.neu.edu>
2021-03-13 04:58:55 -0500

    deploy fix!

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ env: "dev"
 
 # Configure your database
 config :hw07, Hw07.Repo,
-  username: "abbycarr",
+  username: "hw07",
   password: "yii7Ooxeegel",
   database: "hw07_prod",
   hostname: "localhost",
